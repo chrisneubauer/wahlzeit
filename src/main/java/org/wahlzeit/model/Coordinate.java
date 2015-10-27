73f8378fb044b59736d5b624624a4ec8bcd64ba8
Christoph Neubauer <neubauer.office@gmail.com>
2015-10-27 10:42:40 +0100

    included security check for latitude and longitude and extended tests

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -41,20 +41,47 @@ public class Coordinate {
         this.latitude = latitude;
     }
 
-    public double getDistance(Coordinate coordinate) {
-        double dlon = coordinate.getLongitude() - this.getLongitude();
-        double dlat = coordinate.getLatitude() - this.getLatitude();
-        double a = Math.pow((Math.sin(dlat/2)), 2) + Math.cos(this.getLatitude()) * Math.cos(coordinate.getLatitude()) * Math.pow((Math.sin(dlon/2)), 2);
-        double c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a) );
-        double d = 6373 * c; // R is the radius of the Earth
-        return d;
+    public double getDistance(Coordinate coordinate) throws IllegalArgumentException {
+        double lat1 = this.getLatitude();
+        double lat2 = coordinate.getLatitude();
+        double long1 = this.getLongitude();
+        double long2 = coordinate.getLongitude();
+
+        if (lat1 > 90 || lat1 < -90 || lat2 > 90 || lat1 < -90) {
+            throw new IllegalArgumentException("Latitude can only be between -90 and 90 degreees.");
+        }
+        else if (long1 > 180 || long1 < -180 || long2 > 180 || long2 < -180) {
+            throw new IllegalArgumentException("Longitude can only be between -180 and 180 degrees.");
+        }
+        else {
+            double dlon = long2 - long1;
+            double dlat = lat2 - lat1;
+            double a = Math.pow((Math.sin(dlat / 2)), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow((Math.sin(dlon / 2)), 2);
+
+            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            return 6373 * c; // R is the radius of the Earth
+        }
     }
 
-    public double getLatitudinalDistance(Coordinate coordinate) {
-        return Math.abs(this.getLatitude() - coordinate.getLatitude());
+    public double getLatitudinalDistance(Coordinate coordinate) throws IllegalArgumentException {
+        double lat1 = this.getLatitude();
+        double lat2 = coordinate.getLatitude();
+        if (lat1 > 90 || lat1 < -90 || lat2 > 90 || lat1 < -90) {
+            throw new IllegalArgumentException("Latitude can only be between -90 and 90 degreees.");
+        }
+        else {
+            return Math.abs(lat1 - lat2);
+        }
     }
 
-    public double getLongitudinalDistance(Coordinate coordinate) {
-        return Math.abs(this.getLongitude() - coordinate.getLongitude());
+    public double getLongitudinalDistance(Coordinate coordinate) throws IllegalArgumentException {
+        double long1 = this.getLongitude();
+        double long2 = coordinate.getLongitude();
+        if (long1 > 180 || long1 < -180 || long2 > 180 || long2 < -180) {
+            throw new IllegalArgumentException("Longitude can only be between -180 and 180 degrees.");
+        }
+        else {
+            return Math.abs(long1 - long2);
+        }
     }
 }
--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -12,11 +12,15 @@ public class CoordinateTest {
 
     private Coordinate testCoords;
     private Coordinate testCoords2;
+    private Coordinate testCoords3;
+    private Coordinate testCoords4;
 
     @Before
     public void setUpCoordinate() {
-        testCoords = new Coordinate(21.1, 1554.234);
-        testCoords2 = new Coordinate(11.3, 240.11);
+        testCoords = new Coordinate(21.1, 54.234);
+        testCoords2 = new Coordinate(11.3, 140.11);
+        testCoords3 = new Coordinate(210.1, 1554.234);
+        testCoords4 = new Coordinate(-211.3, 240.11);
     }
     @Test
     public void testLatitudinalDistance() {
@@ -32,7 +36,7 @@ public class CoordinateTest {
         double path = testCoords.getLongitudinalDistance(testCoords2);
         double pathReversed = testCoords2.getLongitudinalDistance(testCoords);
 
-        Assert.assertEquals(1314.12, path, 0.2);
+        Assert.assertEquals(85.876, path, 0.2);
         Assert.assertEquals(path, pathReversed, 0.2);
     }
 
@@ -41,7 +45,34 @@ public class CoordinateTest {
         double path = testCoords.getDistance(testCoords2);
         double pathReversed = testCoords2.getDistance(testCoords);
 
+        Assert.assertEquals(14509.90, path, 0.9);
+        Assert.assertEquals(path, pathReversed, 0.2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalculateDistanceException() {
+        double path = testCoords3.getDistance(testCoords4);
+        double pathReversed = testCoords4.getDistance(testCoords3);
+
         Assert.assertEquals(16526.99, path, 0.9);
+        Assert.assertEquals(path, pathReversed, 0.2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLongitudinalDistanceException() {
+        double path = testCoords3.getLongitudinalDistance(testCoords4);
+        double pathReversed = testCoords4.getLongitudinalDistance(testCoords3);
+
+        Assert.assertEquals(1314.12, path, 0.2);
+        Assert.assertEquals(path, pathReversed, 0.2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testLatitudinalDistanceException() {
+        double path = testCoords3.getLatitudinalDistance(testCoords4);
+        double pathReversed = testCoords4.getLatitudinalDistance(testCoords3);
+
+        Assert.assertEquals(9.8, path, 0.2);
         Assert.assertEquals(path, pathReversed, 0.2);
     }
 }
